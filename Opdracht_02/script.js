13463b622f3e19de3c5daed705849c32860d8dd5
Pookoloco <90471681+Pookoloco@users.noreply.github.com>
2021-10-14 18:55:28 +0200

    Add files via upload

---
+++ Opdracht_02/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy Studenten")
+// Dit is een grote som 
+// Line comments worden genegeerd 
+/*  let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division); */
+// Ook block comments worden genegeerd
